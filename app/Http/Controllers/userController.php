1db55b0603379763d799930e27b628677e0b5e7f
Aniruddha Chakraborty <aniruddha.app@gmail.com>
2016-04-06 22:35:08 +0600

    updated data

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Models\API\V1\ComplainModel;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    protected $user_table;
+
+    public function __construct(){
+
+        $this->user_table = new Controller
+
+    }
+
+}
