e745fcfc964892a5781405945d7677f10f120b83
porimol <p.c_roy@yahoo.com>
2016-04-06 23:37:33 +0600

    complain request processor

--- app/Http/Controllers/API/V1/ComplainController.php
+++ app/Http/Controllers/API/V1/ComplainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Models\API\V1\ComplainModel;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\ComplainRequest;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -18,34 +19,49 @@ class ComplainController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ComplainRequest $request)
     {
-        //
+        $message = []; // empty array
+
+        try {
+            $this->complain_table->complainer_name = $this->complainer_name;
+            $this->complain_table->complainer_mobile = $request->complainer_mobile;
+            $this->complain_table->token_id = $request->token_id;
+            $this->complain_table->vat_reg_id = $request->vat_reg_id;
+            $this->complain_table->institute_name = $request->institute_name;
+            $this->complain_table->address = $request->address;
+            $this->complain_table->complain = $request->complain;
+            $this->complain_table->complain_type = $request->complain_type;
+            $result = $this->complain_table->save();
+
+            if($result) {
+                // set the message
+                $message['message'] = 'Medicine(s) added to my dispensary/wish list.';
+                $message['status_code'] = 201;
+            } else{
+                // set the message
+                $message['message'] = 'Medicine can\'t added to my dispensary/wish list!';
+                $message['status_code'] = 422;
+            }
+
+        } catch(Exception $ex) {
+            // set the flash message
+            $message['message'] = $ex->getMessage();
+            $message['status_code'] = $ex->getCode();
+
+        } finally {
+            return response()->json(
+                [
+                    'message' => $message
+                ],
+                $message['status_code']
+            );
+        }
     }
 
     /**
@@ -60,43 +76,10 @@ class ComplainController extends Controller
 
         if($complain)
         {
-            return response()->json(['data' => $complain, 'status_code' => 200], 200);
+            //return response()->json(['data' => $complain, 'status_code' => 200], 200);
+            return response()->json([$complain], 200);
         } else{
             return response()->json(['message' => 'Token ID doesn\'t match!', 'status_code' => 400], 400);
         }
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
